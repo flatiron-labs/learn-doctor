94cdeffef8a2e16f58e0dedfdc232b2d88c7b946
Logan Hasson <logan.hasson@gmail.com>
2015-06-11 18:22:35 -0400

    Don't use Open3 any more

--- lib/learn_doctor.rb
+++ lib/learn_doctor.rb
@@ -3,7 +3,6 @@ require 'faraday'
 require 'colorize'
 require 'fileutils'
 require 'tempfile'
-require 'open3'
 require 'learn_web'
 
 require 'learn_doctor/version'
--- lib/learn_doctor/health_check/step_installer.rb
+++ lib/learn_doctor/health_check/step_installer.rb
@@ -1,12 +1,14 @@
 module LearnDoctor
   class HealthCheck
     class StepInstaller
-      attr_reader   :step, :title
+      attr_reader   :step, :title, :sudo, :password_required
       attr_accessor :file, :result
 
       def initialize(step)
         @step = step
         @title = step[:title]
+        @sudo = step[:sudo]
+        @password_required = step[:password]
       end
 
       def execute
@@ -21,12 +23,20 @@ module LearnDoctor
       private
 
       def set_file
+        print "Installing #{title}..."
         self.file = LearnDoctor::HealthCheck::File.new(step, :install)
       end
 
       def run_install_for_step
-        print "Installing #{title}..."
-        self.result = Open3.popen3('bash', file.path)[1].read.strip
+        if password_required
+          puts "Your password is required to install #{title}."
+          print 'Password: '
+          password = gets.chomp
+
+          self.result = `#{file.path} #{password}`
+        else
+          self.result = `#{sudo ? 'sudo ' : ''}#{file.path}`.strip
+        end
       end
 
       def print_result
